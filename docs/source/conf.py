2c403f25c87fc191f4913289405f0a7e09639cef
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2022-03-02 12:25:20 +0100

    Change path to req.txt file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Alexandre Bohyn"
 author = "Alexandre Bohyn"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.4.0"
 
 
 # -- General configuration ---------------------------------------------------
